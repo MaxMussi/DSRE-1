9f49908580922dae0503439b4f351cff2e309cce
Max_Mussi <maxmussi151@gmail.com>
2026-04-10 09:25:14 -0300

    Menu

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,18 +1,32 @@
 extends Node
 
-@onready var menu = $Menu
-@onready var game = $Game
+@onready var tetris_node = $Game
+@onready var menu_node = $Menu
 
-func _ready():
-	game.hide()
-	game.process_mode = Node.PROCESS_MODE_DISABLED
-	menu.show()
-	
-	menu.start_game_requested.connect(_on_start_game)
+func _ready() -> void:
+	menu_node.start_pressed.connect(_on_start_game)
+	menu_node.exit_pressed.connect(_on_exit_game)
 
-func _on_start_game():
-	menu.hide()
-	menu.process_mode = Node.PROCESS_MODE_DISABLED
-	
-	game.show()
-	game.process_mode = Node.PROCESS_MODE_INHERIT
+	tetris_node.hide()
+	menu_node.show()
+	tetris_node.game_running = false
+
+func _on_start_game() -> void:
+	tetris_node.show()
+	menu_node.hide()
+	tetris_node.new_game()
+
+func _on_exit_game() -> void:
+	get_tree().quit()
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		if not menu_node.visible:
+			_return_to_menu()
+		else:
+			_on_start_game()
+
+func _return_to_menu() -> void:
+	tetris_node.game_running = false
+	menu_node.show()
+	tetris_node.hide()
--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -1,9 +1,59 @@
 extends TileMap
 
-signal start_game_requested
+signal start_pressed
+signal exit_pressed
 
-func _ready():
-	start.call_deferred()
+var current_choice: int = 0
+const ARROW_COORD := Vector2i(1, 3)
+const source_id := 2 
 
-func start():
-	start_game_requested.emit()
+@onready var fg_tilemap: TileMapLayer = $Fg
+
+const OPTION_POSITIONS = [
+	Vector2i(19, 7),
+	Vector2i(19, 9),
+	Vector2i(19, 11)
+]
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	update_arrow_display()
+
+func _input(_event: InputEvent) -> void:
+	if not visible: 
+		return
+
+	if Input.is_action_just_pressed("ui_up"):
+		current_choice = posmod(current_choice - 1, 3)
+		update_arrow_display()
+	
+	elif Input.is_action_just_pressed("ui_down"):
+		current_choice = posmod(current_choice + 1, 3)
+		update_arrow_display()
+	
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection()
+
+func update_arrow_display() -> void:
+	for pos in OPTION_POSITIONS:
+		fg_tilemap.erase_cell(pos)
+	
+	fg_tilemap.set_cell(OPTION_POSITIONS[current_choice], source_id, ARROW_COORD)
+
+func handle_selection() -> void:
+	match current_choice:
+		0: _on_start_selected()
+		1: _on_check_selected()
+		2: _on_exit_selected()
+
+
+func _on_start_selected() -> void:
+	hide()
+	start_pressed.emit()
+
+func _on_check_selected() -> void:
+	print("Check handled by you!") 
+
+func _on_exit_selected() -> void:
+	exit_pressed.emit()
+	get_tree().quit()
